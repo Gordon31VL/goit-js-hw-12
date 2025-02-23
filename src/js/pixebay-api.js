95de098d9202746df88b543a04d3225bc9c37166
GordonVL <ladaniv123@gmail.com>
2025-02-23 14:50:32 +0100

    update

--- src/js/pixebay-api.js
+++ src/js/pixebay-api.js
@@ -1,65 +1,12 @@
 import axios from 'axios';
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-import { getPhotos, gallery } from './render-functions';
-import { loadMoreButton } from './render-functions';
 
-export const searchImageForm = document.querySelector('.searchPhoto');
 
-let page = 1;
-let totalImages = 15;
-let newUserSearching = '';
-
-export async function getData() {
-  
+export async function getData(userSearching, page) {
   try {
-    let userSearching = searchImageForm.inputText.value.trim();
-  
-    if (newUserSearching != userSearching) {
-      newUserSearching = userSearching;
-      gallery.innerHTML = '';
-      loadMoreButton.style.display = 'none';
-    }
-    
-    
-    if (!userSearching) {
-      iziToast.error({
-        color: '#EF4040',
-        message: 'Error, input field is empty',
-        messageColor: '#FAFAFB',
-        maxWidth: '432',
-        iconColor: '#FAFAFB',
-      });
-      return;
-    }
-    
     const response = await axios.get(
-      `https://pixabay.com/api/?key=48639504-554e542bc8495b3a6c3499497&q=${newUserSearching}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=15`
+      `https://pixabay.com/api/?key=48639504-554e542bc8495b3a6c3499497&q=${userSearching}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=15`
     );
-    
-    if (response.data.totalHits <= totalImages) {
-      loadMoreButton.style.display = 'none';
-      iziToast.info({
-        message: `We're sorry, but you've reached the end of search results.`,
-      });
-    }
-    
-    if (response.data.hits.length === 0) {
-      iziToast.error({
-        color: '#EF4040',
-        message:
-        'Sorry, there are no images matching your search query. Please, try again!',
-        messageColor: '#FAFAFB',
-        maxWidth: '432',
-        iconColor: '#FAFAFB',
-      });
-      
-      return;
-    }
-    
-    page += 1;
-    totalImages += 15;
-    getPhotos(response.data.hits);
+    return response.data
   } catch (error) {
     console.log(error);
   }
--- src/main.js
+++ src/main.js
@@ -1,14 +1,60 @@
-import { getData, searchImageForm } from './js/pixebay-api';
-import { loadMoreButton } from './js/render-functions';
+import { getData } from './js/pixebay-api';
+import { loadMoreButton, gallery, getPhotos } from './js/render-functions';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+
 const loader = document.createElement('span');
 loader.classList.add('loader');
+
+export const searchImageForm = document.querySelector('.searchPhoto');
+export let responseData = {};
+let page = 1;
+let newUserSearching = '';
+let totalImages = 15;
 
 searchImageForm.addEventListener('submit', async event => {
   event.preventDefault();
   document.body.append(loader);
 
   try {
-    await getData();
+    let userSearching = searchImageForm.inputText.value.trim();
+    
+    if (newUserSearching != userSearching) {
+      newUserSearching = userSearching;
+      page = 1;
+      totalImages = 15;
+      gallery.innerHTML = '';
+      loadMoreButton.style.display = 'none';
+    }
+
+    const responseData = await getData(newUserSearching, page);
+
+    if (!userSearching) {
+      iziToast.error({
+        color: '#EF4040',
+        message: 'Error, input field is empty',
+        messageColor: '#FAFAFB',
+        maxWidth: '432',
+        iconColor: '#FAFAFB',
+      });
+      return;
+    }
+
+    if (responseData.hits.length === 0) {
+      iziToast.error({
+        color: '#EF4040',
+        message:
+          'Sorry, there are no images matching your search query. Please, try again!',
+        messageColor: '#FAFAFB',
+        maxWidth: '432',
+        iconColor: '#FAFAFB',
+      });
+
+      return;
+    }
+
+    getPhotos(responseData.hits);
   } catch (error) {
     console.error(error);
   } finally {
@@ -17,16 +63,28 @@ searchImageForm.addEventListener('submit', async event => {
 });
 
 loadMoreButton.addEventListener('click', async event => {
-  event.preventDefault();
   document.body.append(loader);
   loadMoreButton.style.display = 'none';
 
   try {
-    await getData();
+    totalImages += 15;
+    page++;
+    const responseData = await getData(newUserSearching, page);
+
+    if (responseData.totalHits <= totalImages) {
+      loadMoreButton.style.display = 'none';
+      iziToast.info({
+        message: `We're sorry, but you've reached the end of search results.`,
+      });
+
+      return;
+    }
+    
+    getPhotos(responseData.hits);
+    loadMoreButton.style.display = 'block';
   } catch (error) {
     console.error(error);
   } finally {
     document.body.removeChild(loader);
-    loadMoreButton.style.display = 'block';
   }
 });
